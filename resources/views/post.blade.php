fb6f924a0dfce78ba0cba984580b44665227d030
Franck GRENIER <franck.grenier@fiducial.net>
2021-03-03 09:31:18 +0100

    Play around with db seed & view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -22,10 +22,19 @@
 </head>
 <body class="antialiased">
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <h1>My blog posts</h1>
-        <p>
-            {{ $post->body }}
-        </p>
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="justify-center pt-8 sm:justify-start sm:pt-0">
+                <h1>My blog posts</h1>
+            </div>
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="p-6">
+                    <h2>{{ $post->title }}</h2>
+                    <p>
+                        {{ $post->body }}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
