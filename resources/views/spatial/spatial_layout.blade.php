ffc507849531eb5aac14e13b396c6c4f7c6661fc
Franck GRENIER <franck.grenier@fiducial.net>
2021-03-12 18:24:21 +0100

    Play around with Mix and Sass

--- resources/views/spatial/spatial_layout.blade.php
+++ resources/views/spatial/spatial_layout.blade.php
@@ -10,19 +10,20 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="/assets/css/main.css" />
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
 </head>
 <body class="{{ Request()->is('spatial') ? 'landing' : '' }}">
 
 @include('spatial/header')
 @yield('content')
 @include('spatial/footer')
-@
 
 <!-- Scripts -->
 <script src="/assets/js/jquery.min.js"></script>
 <script src="/assets/js/skel.min.js"></script>
 <script src="/assets/js/util.js"></script>
 <script src="/assets/js/main.js"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 
 </body>
 </html>
